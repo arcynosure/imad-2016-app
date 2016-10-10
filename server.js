8a6c3334530fa4f7f996a6f3e84777373fab634b
arcynosure <arcynosure@gmail.com>
2016-10-10 00:47:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ var articles = {
 
 function createTemplate (data) {
 var title = data.title;
-var date = data.heading;
+var date = data.date;
 var heading = data.heading;
 var content = data.content;
 var htmlTemplate = `

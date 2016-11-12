1ec933ff27d48e3932c707de11aeb0c77cf120b8
arcynosure <arcynosure@gmail.com>
2016-11-12 21:42:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,6 +59,9 @@ function loadLoginForm () {
         
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
+            
+            
+            
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
